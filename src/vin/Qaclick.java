9800df045e98df74d0a474937bfae0fbb7d7f2ac
Vinaysimha Varma <vinaysimhayadavali_varma@apple.com>
2021-03-27 21:19:22 -0500

    eleventh

--- src/vin/Qaclick.java
+++ src/vin/Qaclick.java
@@ -24,6 +24,9 @@ public class Qaclick {
 	//public WebDriver driver;
 	@Test(priority=1)
 	void setup() throws MalformedURLException {
+		System.out.println("Setup Method");
+
+		/*
 		System.out.println("In Setup method");
 		String path = System.getProperty("user.dir");
 		System.out.println(path); 
@@ -35,19 +38,26 @@ public class Qaclick {
 
 		driver = new ChromeDriver();
 		Assert.assertTrue(true);
+		*/
 		
 	}
 	
 	@Test(priority=2)
 	void launch() throws InterruptedException {
+		System.out.println("Setup Method");
+		/*
 		System.out.println("Launching qaclick academy website");
 		driver.get("http://www.qaclickacademy.com/");
 		String title = driver.getTitle();
 		Assert.assertEquals(title, "QA Click Academy | Selenium,Jmeter,SoapUI,Appium,Database testing,QA Training Academy");
+ 		*/
 	}
 
 	@Test(priority=3)
 	void login() throws InterruptedException {
+		System.out.println("Login Method");
+
+		/*
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//a[contains(@href,'sign_in')]")).click();
 		System.out.println("Clicked on sign in");
@@ -57,7 +67,7 @@ public class Qaclick {
 		driver.findElement(By.name("commit")).click();
 		String value = driver.findElement(By.xpath("//a[@class='link-below-button']")).getText();
 		Assert.assertEquals(value, "Forgot Password?");
-
+		*/
 		
 	}
 	
@@ -65,11 +75,15 @@ public class Qaclick {
 	
 	@Test(priority=4)
 	void captureText() {
+		System.out.println("Login Method");
+
+		/*
 		System.out.println("Capturing text");
 		driver.findElement(By.xpath("//a[@class='link-below-button']")).click();
 		String text = driver.findElement(By.xpath("//div[@class='main-hero']/h1")).getText();
 		System.out.println(text);
 		Assert.assertEquals(text, "Reset Password");
-	}
+		*/
+		}
 	
 }
